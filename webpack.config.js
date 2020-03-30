2bfd75843069bfff1073b1f2b78093da67d7671d
dorodero <doro_dero_bah@yahoo.co.jp>
2020-03-30 21:39:03 +0900

    webpack-dev-serverの追加

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,13 @@ module.exports = {
     // メインとなるJavaScriptファイル（エントリーポイント）
     entry: './src/index.js',
 
+    // ローカル開発用環境を立ち上げる
+    // 実行時にブラウザが自動的に localhost を開く
+    devServer: {
+        contentBase: 'dist',
+        open: true
+    },
+
     // ファイルの出力設定
     output: {
         //  出力ファイルのディレクトリ名
